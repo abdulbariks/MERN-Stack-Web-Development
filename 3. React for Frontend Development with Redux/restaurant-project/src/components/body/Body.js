3e16f3409e6cd0e8c7462f082811eaccae5fe3d1
Abdul Barik <abdulbarik1997m@gmail.com>
2023-11-10 01:27:51 +0600

    Component & Router

--- 3. React for Frontend Development with Redux/restaurant-project/src/components/body/Body.js
+++ 3. React for Frontend Development with Redux/restaurant-project/src/components/body/Body.js
@@ -1,9 +1,20 @@
 import React from "react";
+import {Route, Redirect} from "react-router-dom"
+import Menu from "./Menu";
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
 
 const Body = () =>{
  return(
     <div>
-        <h1>Body</h1>
+        <Route path="/home" exact component={Home} />
+        <Route path="/menu" exact component={Menu} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/about" exact component={About} />
+        <Redirect from="/" to="/menu" />
+
+        {/* <Menu/> */}
     </div>
  )
 }
---
+++ 3. React for Frontend Development with Redux/restaurant-project/src/components/body/Menu.js
@@ -0,0 +1,69 @@
+import React, {Component} from "react";
+import DISHES from '../../data/dishes.js';
+import MenuItem from "./MenuItem.js";
+import DishDetail from "./DishDetail.js";
+import {Modal, ModalBody, ModalFooter, Button, CardColumns } from "reactstrap";
+
+
+class Menu extends Component{
+     state = {
+        dishes: DISHES,
+        selectedDish:null,
+        modalOpen:false
+     }
+
+     onDishSelect = dish =>{
+      this.setState({
+        selectedDish:dish,
+        modalOpen : !this.state.modalOpen
+      });
+     }
+
+     toggleModal = () =>{
+      this.setState({
+            modalOpen: !this.state.modalOpen
+      })
+     }
+
+  render(){
+      const menu = this.state.dishes.map( item =>{
+        return(
+            <MenuItem dish={item} key={item.id}
+            DishSelect ={()=>this.onDishSelect(item)}
+            />
+
+        );
+      })
+
+      let dishDetail = null;
+      if (this.state.selectedDish != null) {
+        dishDetail = <DishDetail dish = {this.state.selectedDish}/>
+      }
+
+
+    return(
+        <div className="container">
+            <div>
+                  <CardColumns>
+                    {menu}
+                  </CardColumns>
+
+                  <Modal isOpen={this.state.modalOpen} onClick={this.toggleModal}>
+                    <ModalBody>
+                      {dishDetail}
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button color="secondary" onClick={this.toggleModal}>
+                        Close
+                      </Button>
+                    </ModalFooter>
+                  </Modal>
+            </div>
+        </div>
+    );
+  }
+  
+}
+
+
+export default Menu;
